3724a3d512e52bcad5ef428fa769992efaaf5a78
Andre Cirino Portilho <andre.portilho@ilia.digital>
2022-07-13 15:48:19 -0300

    Working with queues

---
+++ src/main/java/org/example/WorkingWithQueues.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class WorkingWithQueues {
+    public static void main(String[] args) {
+        Queue<Person> supermarket = new LinkedList<>();
+        supermarket.add(new Person("Alex", 21));
+        supermarket.add(new Person("Mariam", 18));
+        supermarket.add(new Person("Ali", 40));
+
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.peek());
+        System.out.println(supermarket.poll());
+        System.out.println(supermarket.size());
+        System.out.println(supermarket.peek());
+    }
+
+    static record Person(String nome, int age) {}
+}
